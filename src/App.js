689004183033c37af795a26e3849507daf796fdb
Pooja Prasad <pooja.prasad2@mindtree.com>
2021-03-04 22:45:23 +0530

    contact us page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import {UserForm} from './Components/Login/userLogin';
-import {RepForm} from './Components/Login/representativeLogin';
+import { UserForm } from './Components/Login/userLogin';
+import { RepForm } from './Components/Login/representativeLogin';
+import { InputForm } from "./Components/Contact/InputForm";
 
 const App = () => {
   return (
     <div className="container mt-4">
-        <UserForm />
-        <RepForm />
-  </div>
+      <UserForm />
+      <RepForm />
+      <InputForm />
+    </div>
   );
 };
 
