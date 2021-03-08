3c6c7abe1465ac72d29d2f6e2afe9946262b728c
manurag173@gmail.com <Anurag.Mishra2@mindtree.com>
2021-03-08 14:00:15 +0530

    modified files and up for qa testing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import React from 'react';
+
 // import logo from './logo.svg';
 // import './App.css';
-import './components/Navbar/Navbar.css'
+import './Components/Navbar/Navbar.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/Footer/Footer'
+import Footer from './Components/Footer/Footer'
 import { BrowserRouter, Route } from "react-router-dom";
-import Navbar from './components/Navbar/Navbar';
-import Navigator from './components/Navbar/PageNavigator'
-import Home from './components/Pages/Home'
+import Navbar from './Components/Navbar/Navbar';
+import Navigator from './Components/Navbar/PageNavigator'
+import Home from './Components/Pages/Home'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
        <BrowserRouter>
