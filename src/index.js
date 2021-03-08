bbce5d0d93f9c45fa17fca418b1bc735991b1989
manurag173@gmail.com <Anurag.Mishra2@mindtree.com>
2021-03-08 17:11:08 +0530

    changes saved

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
-=======
-import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> c888d04979e042d393818ee2ca75bef4ed46a08f
 
 ReactDOM.render(<App />, document.getElementById("root"));
