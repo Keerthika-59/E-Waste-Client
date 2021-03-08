0552ceb0ac12b7d75ebaad5556638c890d7257c5
manurag173@gmail.com <Anurag.Mishra2@mindtree.com>
2021-03-08 16:48:35 +0530

    code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import './Components/Navbar/Navbar.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Components/Footer/Footer'
 import { BrowserRouter, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
