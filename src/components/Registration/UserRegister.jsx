75e975ccf4b19bbf841193745682faca9a96a9a3
manurag173@gmail.com <Anurag.Mishra2@mindtree.com>
2021-03-11 15:40:35 +0530

    added some changes

--- src/components/Registration/UserRegister.jsx
+++ src/components/Registration/UserRegister.jsx
@@ -8,7 +8,7 @@ import * as Yup from 'yup';
 const phoneRegExp = /^ ((\\+[1 - 9]{ 1, 4 } [\\-] *)| (\\([0 - 9]{ 2, 3 } \\)[\\-] *)| ([0 - 9]{ 2, 4 })[\\-] *)*? [0 - 9]{ 3, 4 }?[\\-] * [0 - 9]{ 3, 4 }?$ /
 
 const UserRegister = () => {
-
+    
     const [name, setName] = useState('');
     const [number, setNumber] = useState();
     const [email, setEmail]  = useState('');
