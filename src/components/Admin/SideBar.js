973bfaf0a703136ee07a56ea32ac93fc5056f2e1
manurag173@gmail.com <Anurag.Mishra2@mindtree.com>
2021-03-18 16:42:03 +0530

    added admin dashboard

--- src/components/Admin/SideBar.js
+++ src/components/Admin/SideBar.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarContent } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import {Link, Redirect} from 'react-router-dom';
 
 const SideBar = () => {
     return (
-        <div>
-            Welcome to Side Bar Page
-        </div>
+        <>
+            <ProSidebar style = {{position : 'fixed'}} className = "mr-auto">
+                <SidebarHeader className = "mx-auto">
+                    <p> Administrator Dashboard </p>
+                </SidebarHeader>
+
+                <Menu iconShape="square">
+                    <MenuItem > <Link to="/"> <p> Dashboard </p> </Link> </MenuItem>
+                    
+                    <SubMenu title="Representative">
+                        <MenuItem>  Verify Representatives </MenuItem>
+                        <MenuItem> View Representatives  </MenuItem>
+                    </SubMenu>
+
+                    <hr />
+
+                    <MenuItem style={{ color: 'whitesmoke' }}> <p> View Users </p>   </MenuItem>
+                    
+                    <MenuItem style = {{color : 'whitesmoke'}}> <p> View Donations </p>  </MenuItem>
+                    <MenuItem style = {{color : 'whitesmoke'}}> <p>View Completed Activities </p>  </MenuItem>
+                    <MenuItem style = {{color : 'whitesmoke'}}> <p> View Pending Activities </p>  </MenuItem>
+                    <MenuItem style = {{color : 'whitesmoke'}}>  <p> View Messages  </p> </MenuItem>
+
+                </Menu>
+
+            </ProSidebar>;
+
+        </>
     )
 }
 
