b35d39730e3a736fe0ed7d1bf8750c892738cb9f
Keerthika Nimmakayala <keerthika.nimmakayala2@mindtree.com>
2021-03-26 17:04:37 +0530

    unit-testing

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,7 +1,7 @@
 // import './App.css';
 import {React,useEffect} from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
-import CarouselHome from '../HomePage/Carousel/CarouselHome';
+
 import Information from '../HomePage/Information/Information';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
---
+++ src/components/tests/HomePage.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Data from '../HomePage/Information/Data';
+import Dispose from '../HomePage/Information/Dispose.jsx'
+import Donate from '../HomePage/Information/Donate.jsx'
+import Ewaste from '../HomePage/Information/Ewaste.jsx'
+import Information from '../HomePage/Information/Information'
+
+test('renders test Data', () => {
+    const div=document.createElement("div");
+    <Route>
+        ReactDom.render(<Data/>,div);
+    </Route>
+    ReactDOM.unmountComponentAtNode(div);
+   });
+
+   test('renders test Dispose', () => {
+    const div=document.createElement("div");
+    <Route>
+        ReactDom.render(<Dispose/>,div);
+    </Route>
+    ReactDOM.unmountComponentAtNode(div);
+   });
+
+   test('renders test Donate', () => {
+    const div=document.createElement("div");
+    <Route>
+        ReactDom.render(<Donate/>,div);
+    </Route>
+    ReactDOM.unmountComponentAtNode(div);
+   });
+
+   test('renders test Ewaste', () => {
+    const div=document.createElement("div");
+    <Route>
+        ReactDom.render(<Ewaste/>,div);
+    </Route>
+    ReactDOM.unmountComponentAtNode(div);
+   });
+
+   test('renders test Information', () => {
+    const div=document.createElement("div");
+    <Route>
+        ReactDom.render(<Information/>,div);
+    </Route>
+    ReactDOM.unmountComponentAtNode(div);
+   });
