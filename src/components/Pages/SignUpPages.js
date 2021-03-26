bf94574ce9fb564332503b43bc21c12e16584854
Keerthika Nimmakayala <keerthika.nimmakayala2@mindtree.com>
2021-03-26 17:05:15 +0530

    unit-testing

--- src/components/Pages/SignUpPages.js
+++ src/components/Pages/SignUpPages.js
@@ -1,20 +1,15 @@
-import { React, useEffect } from 'react';
-import { Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import {React,useEffect} from 'react';
+import {Button} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 export default function SignUpOptions() {
-    useEffect(() => {
-        window.scrollTo(0, 0)
-    }, [])
-    return ( <
-        >
-        <
-        h3 style = {
-            { marginTop: "10%", marginBottom: "10%", marginLeft: "6%" } } > Please choose a Sign up option < /h3> <
-        Link to = '/UserLogIn' > < Button style = {
-            { marginTop: "10%", marginLeft: "8%", width: "300px" } } > User Sign up < /Button></Link > < br > < /br> <
-        Link to = '/RepresentativeLogIn' > < Button style = {
-            { marginTop: "10%", marginLeft: "8%", width: "300px" } } > Representative Sign up < /Button></Link >
-        <
-        />
-    );
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+  return (
+    <>
+    <h3 style={{marginTop:"10%",marginBottom:"10%",marginLeft:"6%"}}>Please choose a Sign up option</h3>
+    <Link to='/UserLogIn'><Button style={{marginTop:"10%",marginLeft:"8%",width:"300px"}}>User Sign up</Button></Link><br></br>
+    <Link to='/RepresentativeLogIn'><Button style={{marginTop:"10%",marginLeft:"8%",width:"300px"}}>Representative Sign up</Button></Link>
+    </>
+  );
 }
