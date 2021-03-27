b8c01b3f9889c03e705b625efdf5a30f6e5821ea
Pooja Prasad <pooja.prasad2@mindtree.com>
2021-03-27 13:36:20 +0000

    Update pending.js

--- src/components/Dashboard/UserDash/pages/pending.js
+++ src/components/Dashboard/UserDash/pages/pending.js
@@ -93,14 +93,14 @@ const Pending = () => {
                       </div>
                       <div class="col">
                         <label for="id">Donation</label>
-                        <div class="">
+                       <div class="form-control">
                           {activity.donation ? (
                             <>
-                              {activity.cl ? "clothes" : ""}{" "}
-                              {activity.el ? "Electronics" : ""}{" "}
-                              {activity.to ? "toys" : ""}{" "}
-                              {activity.fo ? "food" : ""}{" "}
-                              {activity.st ? "stationary" : ""}
+                              <p>{activity.cl ? "Clothes " : ""} 
+                              {activity.el ? "Electronics " : ""}
+                              {activity.to ? "Toys " : ""}
+                              {activity.fo ? "Food " : ""}
+                              {activity.st ? "Stationary " : ""}</p>{" "}
                             </>
                           ) : (
                             "No"
