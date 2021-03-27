4151010241eb5197f6c7c0252fa7444b5fbcb8b3
Pooja Prasad <pooja.prasad2@mindtree.com>
2021-03-27 13:32:44 +0000

    Update pending.js

--- src/components/Dashboard/UserDash/pages/pending.js
+++ src/components/Dashboard/UserDash/pages/pending.js
@@ -47,7 +47,6 @@ const Pending = () => {
           .map((activity, index) => (
             <div
               className="cards px-3 py-2 my-4 mx-4 justify-content-center"
-              style={{ width: "700px" }}
             >
               <div className="card-header bg-white">
                 {/* <h3 className="pendingHeading">Pending activity</h3> */}
